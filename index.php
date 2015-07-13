87b5fb6b60df44b546a8ea121d8ce69c727a0d32
Andrzej270 <bart_28@o2.pl>
2015-07-14 01:28:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,39 +7,40 @@ require_once('eliksirsila.php');
 require_once('Potwor.php');
 require_once('tura.php');
 require_once('Wiedzmin.php');
-do{
-$p1=1;
-$p2=1;	
+function pobranie($tekst,$p){
 $stdout = fopen('php://stdout', 'w');
-fwrite($stdout, "wybierz cztery parametry potwora(jeden po drugim) ");
-fclose($stdout);
-$stdin = fopen('php://stdin', 'r');
-$wybor2=array();
-for($i=1; $i<5; $i++){
-$wybor2[$i]=fgetc($stdin);
-if($wybor2[$i]==0){
-	echo "\nbrak parametru potwora\n";
-	$p1=0;
-}
-}
-fclose($stdin);
-$stdout = fopen('php://stdout', 'w');
-fwrite($stdout, "wybierz cztery parametry wiedzmina(jeden po drugim) ");
+fwrite($stdout,$tekst);
 fclose($stdout);
 $stdin = fopen('php://stdin', 'r');
 $wybor=array();
-for($i=1; $i<5; $i++){
+for($i=1; $i<$p+1; $i++){
 $wybor[$i]=fgetc($stdin);
-if($wybor[$i]==0){
-	echo "\nbrak parametru wiedzmna\n";
-	$p2=0;
 }
-}
+return $wybor;
 fclose($stdin);
 }
-while($p1==0 || $p2==0);
+$tekst1="wybierz cztery parametry potwora(jeden po drugim)";
+$tekst2="wybierz cztery parametry wiedzmina(jeden po drugim)";
+$tekst3="\nTwoja tura jaki ruch wykonasz(a-atak b-stworzenie eliksiru c-wypicie d-obrona e-koniec tury)\n";
+$tekst4="Podaj poziom eliksiru";
+do{
+$wybor=array();	
+$wybor2=array();
+$wybor2=pobranie($tekst1,4);
+$wybor=pobranie($tekst2,4);
+$brak=false;
+for($i=1; $i<5; $i++){
+if($wybor2[$i]==0 || $wybor[$i]==0){
+	
+	echo "\nbrak parametru prosze podac jeszcze raz zmienne\n";
+	$brak=true;
+}
+}
+}
+while($brak);
 $zmienna3=new potwor($wybor2[1],$wybor2[2],$wybor2[3],$wybor2[4]);
 $zmienna2=new Wiedzmin($wybor[1],$wybor[2],$wybor[3],$wybor[4]);
+
 $koniec=false;
 $zmienna= new tura($zmienna2,$zmienna3);
 do{	
@@ -51,54 +52,56 @@ while($wtura==0){
 echo $zmienna2;
 echo"\n";	
 echo $zmienna3;
-$stdout = fopen('php://stdout', 'w');
-fwrite($stdout, "\nTwoja tura jaki ruch wykonasz(a-atak b-stworzenie eliksiru c-wypicie d-obrona e-koniec tury) ");
-fclose($stdout);
-$stdin = fopen('php://stdin', 'r');
-$wybor=fgetc($stdin);
-fclose($stdin);
 
-if($wybor=="a"){
+$wyb=array();
+$wyb=pobranie($tekst3,1);
+
+$zmienna2->punktyakcji();
+$w=$wyb[1];
+
+switch($w){
+case "a": 
 $zrecznosc=$zmienna3->getzrecz();	
-$atak=$zmienna2->atak($zrecznosc);
-if($atak==true){ 
-$zmienna3->trafiony();
-echo"\n Potwor trafiony\n";}
-else{echo "pudlo";}
-}
+$zmienna2->atak($zmienna3,$zrecznosc);
+break;
 
-elseif($wybor=="b"){
-$stdout = fopen('php://stdout', 'w');	
-fwrite($stdout,"Podaj poziom eliksiru");
-fclose($stdout);
-$stdin = fopen('php://stdin', 'r');
-$poziom=fgetc($stdin);
-fclose($stdin);
-if($zmienna2->getpktakcji()>1+$poziom){
+case "b":
+$wyn=array();
+$wyn=pobranie($tekst4,1);
+$poziom=$wyn[1];
+if($zmienna2->getpktakcji()>=$poziom+1){
 $typ=$zmienna2->utwurzeliksir();
+
 $eliksir=new $typ($poziom,$zmienna2);
 $zmienna2->seteliksir($eliksir,$poziom);
 echo"\nEliksir utworzony\n";
 }
 else{echo"za malo punktow akcji";}	
-}
+break;
 
 
-elseif($wybor=="c"){
+case"c":
 $zmienna2->wypijeliksir();
 echo"\nEliksir wypity\n";
-}
+break;
 
-elseif($wybor=="d")	{
+case"d":
 $zmienna2->obrona();
 $zmienna2->koniectury();
 $wtura=1;
-}
+break;
 
 
-elseif($wybor=="e"){
+
+case"e":
 $zmienna2->koniectury();
 $wtura=1;
+break;
+
+
+default:
+echo"\nProsze zaznaczyc poprawna literke\n";
+break;
 }
 if($zmienna3->getzycie()<=0){
 	echo "koniec gry wygrales";
@@ -109,11 +112,7 @@ if($zmienna3->getzycie()<=0){
 }
 elseif($zmienna3->gettura()==1){
 $wzrecznosc=$zmienna2->getzrecz();	
-$atak=$zmienna3->atak($wzrecznosc);
-if($atak==true){ 
-$zmienna2->trafiony();
-echo"Wiedzmin trafiony";}
-else{echo "\n pudlo";}
+$zmienna3->atak($zmienna2,$wzrecznosc);
 $zmienna3->koniectury();
 if($zmienna2->getzycie()<=0){
 	echo "koniec gry przegrales";
